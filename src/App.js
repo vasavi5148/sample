5e5adaae0c300462852cb083a8d7c818741f042f
vasavi <vasavi.neelisetty@zemosolabs.com>
2023-01-30 08:41:05 +0530

    add data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  componentDidMount() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div className="counter">
+        <h1 className="count">{this.state.count}</h1>
+
+        <button type="button" onClick={this.handleIncrement}>
+          Increment
+        </button>
+        <button type="button" onClick={this.handleDecrement}>
+          Decrement
+        </button>
+        <input type="text"></input>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Counter from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Component1 from "./usecontext";
+import CounterExample from "./useeffect";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Counter />
+    <Component1 />
+    <CounterExample />;
   </React.StrictMode>
 );
 
